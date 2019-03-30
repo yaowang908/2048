92b2343ea8da77022e81eec6469f040148e8834d
Yao Wang <yao422321065@yahoo.com>
2019-03-30 19:00:43 -0400

    1.0.0 build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/2048/build/precache-manifest.92694ecb559dc5a1c477e5916d098ca8.js"
+  "/2048/precache-manifest.a594d1dc20eac64d2d8979324331b06e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/2048/build/index.html", {
+workbox.routing.registerNavigationRoute("/2048/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
